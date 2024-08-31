e49c4370b0742975c98c7db92a8d866aaad243b7
Praveen8160 <praveenp8160@gmail.com>
2024-08-31 18:16:17 +0530

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <h1>Blood bank</h1>
+    </>
+  );
+}
+
+export default App;
