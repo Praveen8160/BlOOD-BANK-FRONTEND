759802fade21ee5e8208f89beb25a3a895196c91
Praveen8160 <praveenp8160@gmail.com>
2024-10-10 21:30:35 +0530

    update image

--- src/utils/Image.js
+++ src/utils/Image.js
@@ -8,7 +8,7 @@ import first from "../assets/1.jpg";
 import second from "../assets/2.jpg";
 import third from "../assets/3.png";
 import fourth from "../assets/4.jpg";
-import Donor from "../assets/donor.png";
+import Donor from "../assets/Donor.png";
 import BloodBank from "../assets/bloodbank.png";
 const images = {
   process_1,
