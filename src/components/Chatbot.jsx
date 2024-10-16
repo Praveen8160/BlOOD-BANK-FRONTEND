efc14a56335913014a6558fe0f97e6270c1bac1c
Praveen8160 <praveenp8160@gmail.com>
2024-10-16 22:52:36 +0530

    Update cahtbot UI

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,10 +5,9 @@ function Chatbot() {
     <div className="flex bg-slate-600 flex-col items-center justify-center">
       <iframe
         allow="microphone;"
-        width="350"
-        height="430"
-        className="w-screen min-h-[40rem] border-none"
+        className="w-[350px] h-[430px] sm:w-screen sm:min-h-[40rem] border-none"
         src="https://console.dialogflow.com/api-client/demo/embedded/dff4e184-954f-470f-8126-992451bbbf91"
+        title="Chatbot"
       ></iframe>
     </div>
   );
